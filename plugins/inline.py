9b7c7ad27d83d044a0c040d2a794eab95f3fa407
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-11-08 21:08:31 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -23,8 +23,7 @@ async def answer(bot, query):
                 title=file.file_name,
                 file_id=file.file_id,
                 caption=file.caption,
-                description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
-                reply_markup=reply_markup,
+                description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}'
             )
         )
 
