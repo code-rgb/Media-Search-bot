968070146f72c0d10358cf7008850fcacc622445
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-11-08 22:30:59 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -11,7 +11,7 @@ CACHE_TIME = 60
 @Client.on_inline_query()
 async def answer(bot, query):
   """Show search results for given inline query"""
-  if Client.on_inline_query.id in ADMINS:
+  if id in ADMINS:
     results = []
     string = query.query
     reply_markup = get_reply_markup(bot.username)
